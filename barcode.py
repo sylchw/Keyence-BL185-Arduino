e1b10e6cdd977318e76d27a4d57340e4dcaf1787
Chun Hong Wei, Sylvester <chun.hongwei@u.nus.edu>
2019-07-02 18:15:04 +0800

    Update barcode.py
    store serial message as string

--- barcode.py
+++ barcode.py
@@ -9,28 +9,34 @@ GPIO.setup(18,GPIO.OUT)
 state = True
 
 ser = serial.Serial(
-        port=' /dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        "/dev/ttyS0", #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
-#
-#ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
-#ser.baudrate = 9600                     #Set baud rate to 9600
+
 
 mask = 0b01111111
+#hex_mask = 0x10F447
+
 
 while True:
     GPIO.output(18, True)
     time.sleep(0.1)
     
-    c = ser.read()
-    c = ord('a')
-    result = c & mask
-    print(result)
+    message_list = []
     
+    while ser.in_waiting:
+        c = ser.read()
+        convert_to_int = ord(c)
+        result = convert_to_int & mask
+        message_list.append(chr(result))
+        #print(chr(result), end='')
+        
+    message = ''.join(message_list)
+    print(message)
+    print()
     GPIO.output(18,False)
     time.sleep(0.1)
-
